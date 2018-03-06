c7ead7c77758127bf2eb9125b3ddb68e911dcd11
tbaustin <tbaustin1992@gmail.com>
2018-03-06 13:55:36 -0600

    change schema

--- schema/schema.js
+++ schema/schema.js
@@ -11,7 +11,7 @@ const baseURL = 'http://localhost:3000';
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => {
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
